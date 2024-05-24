7bbd3ccf81d7bcdefb6c33ec7b4aab75a259a30d
Tomás F. Medrano <70808947+Tomasmed32@users.noreply.github.com>
2024-05-24 14:51:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,11 @@
-import stremlit
+import streamlit
 
 API_URL = "https://tomas3254-legalbot.hf.space/api/v1/prediction/b21709c5-f495-4db2-9ca5-9f6129441557"
 
-# use form data to upload files
-form_data = {
-    "files": ('examplefile', open('examplefile', 'rb'))
-}
-body_data = {
-    "summarizerPromptName": "example",
-    "responseLang": "example",
-    "maxSummarizedResults": 1,
-}
-
-def query(form_data):
-    response = requests.post(API_URL, files=form_data, data=body_data)
+def query(payload):
+    response = requests.post(API_URL, json=payload)
     return response.json()
-
-output = query(form_data)
+    
+output = query({
+    "question": "Hey, how are you?",
+})
